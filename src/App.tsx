e33956a21e06e67b39a994adeb8022533a451791
Takahiro <tk.iyei@gmail.com>
2020-05-17 08:44:35 +0900

    add 'yarn run fmt' script

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import {TaskList} from './Task';
-import {
-  RecoilRoot,
-} from 'recoil';
-
+import React from "react";
+import { TaskList } from "./Task";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <RecoilRoot>
-      <TaskList/>
+      <TaskList />
     </RecoilRoot>
   );
 }
